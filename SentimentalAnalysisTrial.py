cca543e951b2ccb0a516a42142309e9660e17455
Desingh paal <82135049+desinghpaal@users.noreply.github.com>
2021-05-17 15:31:56 +0530

    Update SentimentalAnalysisTrial.py

--- SentimentalAnalysisTrial.py
+++ SentimentalAnalysisTrial.py
@@ -1,9 +1,9 @@
 import tweepy
 from textblob import TextBlob
-consumer_key = 'RxlvB9EOulCFLO0n7D6B9DNCn'
-consumer_secret = 'V57nNgToH3iip0HXNXqh4Sw3LpYzJbGCMobUI5MaTEhPzj5VEA'
-access_token = '984360223581204480-lSUxqpDhd8wId6EXtfhCIHHhhSlRgi6'
-access_token_secret = '1LVkjjYoua8Aw4lSBIuF8PuA0fHbuhIJNsCkvc9hAGVPD'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api =tweepy.API(auth)
